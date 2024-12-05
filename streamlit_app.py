bc4e7327aaf858639718abf8aebb9e35ec973211
Aunurrofiq Faried Wajdi <86298549+aunurrofiqFaried@users.noreply.github.com>
2024-12-05 09:39:59 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,9 +22,8 @@ option = st.sidebar.selectbox(
 
 if option == 'Deskripsi' or option == '' :
       st.write("# Halaman Deskripsi")
-      st.write("Dataset ini digunakan untuk mendeteksi potensi kebakaran berdasarkan sensor data lingkungan seperti suhu, kelembaban, tekanan, dan gas kimia.")
-      st.write("Berikut adalah statistik dari dataset:")
-      st.write(data.describe())
+      st.write("Fitur prediksi kebakaran dalam aplikasi ini dirancang untuk membantu menganalisis kemungkinan terjadinya kebakaran berdasarkan data yang dihasilkan oleh berbagai sensor lingkungan. Sensor-sensor ini mencakup suhu, kelembaban, tekanan atmosfer, gas kimia seperti H2 dan ethanol, serta indikator gas seperti TVOC (Total Volatile Organic Compounds) dan eCO2 (equivalent Carbon Dioxide). Model machine learning berbasis Random Forest yang telah dilatih digunakan untuk memprediksi apakah alarm kebakaran harus aktif (ON) atau tidak (OFF). Input yang diberikan pengguna akan melalui proses normalisasi menggunakan StandardScaler untuk memastikan prediksi yang akurat. Model ini memberikan notifikasi dini untuk potensi kebakaran, sehingga dapat membantu dalam pengambilan keputusan cepat untuk mitigasi bahaya.")
+      st.write("Narmin Humbatli")
 elif option == 'Dataset' :
       st.write("# Halaman Dataset")
       st.write("### Tabel Dataset")
@@ -49,17 +48,17 @@ elif option == 'Prediksi':
       col1, col2, col3 = st.columns(3)
 
       with col1:
-            temp = st.number_input('Temperature [C]', value=20.0, min_value=-10.0, max_value=50.0, help="Masukkan suhu dalam derajat Celcius.")
-            hum = st.number_input('Humidity [%]', value=57.36, min_value=0.0, max_value=100.0, help="Masukkan kelembaban relatif dalam persen.")
+            temp = st.slider('Temperature [C]', min_value=-10.0, max_value=50.0, value=20.0, step=0.1, help="Atur suhu dalam derajat Celcius.")
+            hum = st.slider('Humidity [%]', min_value=0.0, max_value=100.0, value=57.36, step=0.1, help="Atur kelembaban relatif dalam persen.")
       
       with col2:
-            tvoc = st.number_input('TVOC [ppb]', value=0, min_value=0, max_value=10000, help="Masukkan konsentrasi Total Volatile Organic Compounds.")
-            eco2 = st.number_input('eCO2 [ppm]', value=400, min_value=0, max_value=5000, help="Masukkan konsentrasi Carbon Dioxide equivalent.")
+            tvoc = st.slider('TVOC [ppb]', min_value=0, max_value=10000, value=0, step=1, help="Atur konsentrasi Total Volatile Organic Compounds.")
+            eco2 = st.slider('eCO2 [ppm]', min_value=0, max_value=5000, value=400, step=1, help="Atur konsentrasi Carbon Dioxide equivalent.")
 
       with col3:
-            h2 = st.number_input('Raw H2', value=12306, min_value=0, help="Masukkan nilai mentah sensor H2.")
-            eth = st.number_input('Raw Ethanol', value=18520, min_value=0, help="Masukkan nilai mentah sensor Ethanol.")
-            press = st.number_input('Pressure [hPa]', value=939.735, min_value=900.0, max_value=1100.0, help="Masukkan tekanan atmosfer dalam hPa.")
+            h2 = st.slider('Raw H2', min_value=0, max_value=30000, value=12306, step=1, help="Atur nilai mentah sensor H2.")
+            eth = st.slider('Raw Ethanol', min_value=0, max_value=30000, value=18520, step=1, help="Atur nilai mentah sensor Ethanol.")
+            press = st.slider('Pressure [hPa]', min_value=900.0, max_value=1100.0, value=939.735, step=0.1, help="Atur tekanan atmosfer dalam hPa.")
 
       if st.button('Predict'):
             data_input = np.array([[temp, hum, tvoc, eco2, h2, eth, press]])
